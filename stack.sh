e718a44e9ba35c9c2fcf4008426167cdbe5b86ef
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2016-08-23 04:01:00 +0200

    improve error handling + improve var shadowing prevention

--- stack.sh
+++ stack.sh
@@ -3,59 +3,53 @@
 # Distributed under the terms of the BSD 3-Clause licence
 
 __stack::size() {
-    local _stack_id="${1?"${FUNCNAME}: error"}"
-    local _size
-    eval "${2:-"_size"}=\"\${#${_stack_id}[@]}\""
-    [[ -n "${2}" ]] || printf '%d\n' "${_size}"
+    local __size_sid="${1?"${FUNCNAME}: error"}" __size_res_var="${2}"
+    local __size_sz
+
+    eval "${__size_res_var:-"__size_sz"}=\"\${#${__size_sid}[@]}\""
+    (($?)) && { echo "${FUNCNAME}: Error: assign returned an error" >&2; return 1; }
+
+    [[ -n "${__size_res_var}" ]] || printf '%s\n' "${__size_sz}"
 }
 
 __stack::push() {
-    local _stack_id="${1?"${FUNCNAME}: error"}"
+    local __push_sid="${1?"${FUNCNAME}: error"}"
     shift
-
-    local _i='' _item
-    __stack::size "${_stack_id}" _i
-
-    for _item in "${@}" ; do
-        eval "${_stack_id}[${_i}]='${_item}'"
-        ((_i++))
-    done
-
-    return 0
+    eval "${__push_sid}+=( \"\${@}\" )"
+    (($?)) && { echo "${FUNCNAME}: Error: assign returned an error" >&2; return 1; }
 }
 
 __stack::top() {
-    local __sid="${1?"${FUNCNAME}: error"}" __rv="${2}"
-    local __i=-1
-    __stack::size "${__sid}" __i
+    local __top_sid="${1?"${FUNCNAME}: error"}" __top_res_var="${2}"
+    local __top_i=-1 __top_val
+
+    __stack::size "${__top_sid}" __top_i
     # bash arrays are indexed from zero
-    ((--__i))
+    ((--__top_i < 0)) && \
+        { echo "${FUNCNAME}: Error: stack is empty" >&2; return 1; }
 
-    local __vl
-    eval "__vl=\"\${${__sid}[${__i}]}\""
+    eval "${__top_res_var:-"__top_val"}=\"\${${__top_sid}[${__top_i}]}\""
+    (($?)) && { echo "${FUNCNAME}: Error: assign returned an error" >&2; return 1; }
 
-    if [[ -n "${__rv}" ]] ; then
-        eval "${__rv}=\"\${__vl}\""
-    else
-        printf '%s\n' "${__vl}"
-    fi
-
-    return 0
+    [[ -n "${__top_res_var}" ]] || printf '%s\n' "${__top_val}"
 }
 
 __stack::pop() {
-    local __sid="${1?"${FUNCNAME}: error"}" _res_var="${2}"
-    local __i=-1
-    __stack::size "${__sid}" __i
+    local __pop_sid="${1?"${FUNCNAME}: error"}" __pop_res_var="${2}"
+    local __pop_i=-1
+    __stack::size "${__pop_sid}" __pop_i
     # bash arrays are indexed from zero
-    ((--__i))
+    ((--__pop_i < 0)) && \
+        { echo "${FUNCNAME}: Error: stack is empty" >&2; return 1; }
 
-    local item
-    __stack::top "${__sid}" "${_res_var:-"item"}"
+    local __pop_val
+    __stack::top "${__pop_sid}" "${__pop_res_var:-"__pop_val"}"
+    (($?)) && { echo "${FUNCNAME}: Error: ::top returned an error" >&2; return 1; }
 
-    unset ${__sid}[${__i}]
+    unset ${__pop_sid}[${__pop_i}]
+    (($?)) && { echo "${FUNCNAME}: Error: unset returned an error" >&2; return 1; }
 
-    [[ -n "${_res_var}" ]] || printf '%s\n' "${item}"
+    [[ -n "${__pop_res_var}" ]] || printf '%s\n' "${__pop_val}"
 }
 
 __stack::get_methods() {
@@ -63,42 +57,44 @@ __stack::get_methods() {
 }
 
 __stack::destroy() {
-    local __sid="${1?"${FUNCNAME}: error"}"
+    local __destroy_sid="${1?"${FUNCNAME}: error"}"
 
-    ##destroy wrapper funcs
-    local methods=( $(__stack::get_methods) )
-    unset -f "${methods[@]/#/"${__sid}."}"
+    ## destroy wrapper funcs
+    local __destroy_methods=( $(__stack::get_methods) )
+    unset -f "${__destroy_methods[@]/#/"${__destroy_sid}."}"
+    (($?)) && { echo "${FUNCNAME}: Error: unsetting functions returned an error" >&2; return 1; }
 
     ## destroy the variable holding our stack
-    unset "${__sid}"
+    unset "${__destroy_sid}"
+    (($?)) && { echo "${FUNCNAME}: Error: unsetting __destroy_sid returned an error" >&2; return 1; }
 }
 
 stack::new() {
-    local _rvar="${1?"${FUNCNAME}: error"}"
-    local __stack_id
+    local __new_ret_var="${1?"${FUNCNAME}: error"}"
+    local __new_sid
 
     ## generate stack id
     while (( 1 )) ; do
-        __stack_id="__stack_oop_${FUNCNAME[1]}_$((RANDOM*RANDOM))"
-        if ! declare -p "${__stack_id}" &>/dev/null ; then
+        __new_sid="__stack_oop_${FUNCNAME[1]}_$((RANDOM*RANDOM))"
+        if ! declare -p "${__new_sid}" &>/dev/null ; then
             break
         fi
     done
 
     # create array holding our stack
-    eval "declare -g -a ${__stack_id}=()"
+    eval "declare -g -a ${__new_sid}=()"
 
     ## generate wrappers
-    local m
-    for m in $(__stack::get_methods) ; do
+    local __new_m
+    for __new_m in $(__stack::get_methods) ; do
         eval "
-            ${__stack_id}.${m}(){
-                __stack::${m} '${__stack_id}' \"\${@}\"
+            ${__new_sid}.${__new_m}(){
+                __stack::${__new_m} '${__new_sid}' \"\${@}\"
             }
         "
     done
 
     ## And finally assign the id to the variable.
-    ## This will allow calling functions like `${_rvar}.method`
-    eval "${_rvar}='${__stack_id}'"
+    ## This will allow calling functions like `${__new_ret_var}.method`
+    eval "${__new_ret_var}='${__new_sid}'"
 }
